5944042e09ac4f146ff3df93129fa5455faedb9e
mygithub724 <venimyna@gmail.com>
2023-01-18 17:34:12 +0530

    copy data from one file to snother file using files concept fgetc(),fputc()

---
+++ file_copy.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(int argc,char **argv)
+{
+	if(argc!=2)
+	{
+		printf("usage :.\a.out file \n");
+		return 0;
+	}
+	FILE *fs=fopen(argv[1],"r");
+	if(fs==NULL)
+	{
+		printf("Source file does not exit\n");
+		return 0;
+	}
+	char ch;
+	FILE *fd=fopen("data.txt","w");
+	while((ch=getc(fs))!=-1)
+	{
+		fputc(ch,fd);
+	}
+}
